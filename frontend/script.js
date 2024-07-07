604958aa62f690df7a67f356fab48242a920c48a
nico dominguez <nadominguez@fi.uba.ar>
2024-07-07 00:12:09 -0300

    Solucion de bugs y marcar productos sin stock

--- frontend/script.js
+++ frontend/script.js
@@ -8,74 +8,91 @@ function obtenerIdUsuario() {
 
 // Función para agregar productos al carrito
 function addToCart(name, price) {
-    let cart = JSON.parse(localStorage.getItem('cart')) || [];
-    let item = { name: name, price: price };
+    let cart = JSON.parse(localStorage.getItem('carrito')) || [];
+    let item = { nombre: name, precio: price };
     cart.push(item);
-    localStorage.setItem('cart', JSON.stringify(cart));
+    localStorage.setItem('carrito', JSON.stringify(cart));
+    console.log('Carrito actualizado:', cart); 
     alert('Producto agregado al carrito');
 }
 
 // Función para cargar el carrito y mostrar los productos
 function loadCart() {
-    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+    const cart = JSON.parse(localStorage.getItem('carrito')) || [];
+    console.log('Cargando carrito:', cart); 
     const cartItems = document.getElementById('carrito-items');
     cartItems.innerHTML = '';
 
     let subtotal = 0;
 
     cart.forEach((item, index) => {
+        console.log('Item en carrito:', item); 
         const div = document.createElement('div');
         div.classList.add('carrito-item');
         div.innerHTML = `
             <div>
-                <h4>${item.name}</h4>
-                <p>Precio: $${item.price.toFixed(2)}</p>
+                <h4>${item.nombre}</h4>
+                <p>Precio: $${item.precio.toFixed(2)}</p>
                 <button onclick="removeFromCart(${index})">Eliminar</button>
             </div>
         `;
         cartItems.appendChild(div);
-        subtotal += item.price;
+        subtotal += item.precio;
     });
 
-    const impuestos = subtotal * 0.21; // Suponiendo un impuesto del 21%
-    const total = subtotal + impuestos;
+    //const impuestos = subtotal * 0.21; //Suponiendo  un impuesto del 21%
+    //const total = subtotal + impuestos;
 
-    document.getElementById('subtotal').innerText = `Subtotal: $${subtotal.toFixed(2)}`;
-    document.getElementById('impuestos').innerText = `Impuestos: $${impuestos.toFixed(2)}`;
-    document.getElementById('total').innerText = `Total: $${total.toFixed(2)}`;
+    document.getElementById('subtotal').innerText = ` $${subtotal.toFixed(2)}`;
+    //document.getElementById('impuestos').innerText = ` $${impuestos.toFixed(2)}`;
+    document.getElementById('total').innerText = ` $${subtotal.toFixed(2)}`;
 }
 
 // Función para eliminar productos del carrito
 function removeFromCart(index) {
-    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    let cart = JSON.parse(localStorage.getItem('carrito')) || [];
     cart.splice(index, 1);
-    localStorage.setItem('cart', JSON.stringify(cart));
+    localStorage.setItem('carrito', JSON.stringify(cart));
     loadCart();
 }
 
 // Función para procesar el pago
 function procesarPago() {
-    const carrito = JSON.parse(localStorage.getItem('cart')) || [];
+    const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
     if (carrito.length === 0) {
         alert('El carrito está vacío.');
         return;
     }
-    localStorage.removeItem('cart');
-    alert('Compra realizada exitosamente');
-    loadCart();
+
+    var idUsuario = obtenerIdUsuario();
+
+    fetch('http://127.0.0.1:5000/procesar_pago/' + idUsuario, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(carrito)
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            localStorage.removeItem('carrito');
+            alert('Compra realizada exitosamente');
+            loadCart();
+        } else {
+            alert(data.message);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Hubo un error al procesar el pago.');
+    });
 }
 
 // Cargar el carrito al cargar la página del carrito
 if (document.getElementById('carrito-items')) {
     window.onload = loadCart;
 }
-
-// Event listener para el botón de "Proceder al pago"
-if (document.getElementById('checkout')) {
-    document.getElementById('checkout').addEventListener('click', procesarPago);
-}
-
-
 
 document.addEventListener('DOMContentLoaded', function() {
     let slideIndex = 0;
